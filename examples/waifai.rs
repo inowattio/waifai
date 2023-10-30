7fc5ab0e720af8c042be752083b64e92b173acb6
CosminPerRam <cosmin.p@live.com>
2023-10-30 13:06:09 +0200

    Fix example using String instead of &str

--- examples/waifai.rs
+++ examples/waifai.rs
@@ -6,12 +6,12 @@ fn main() -> Result<(), WFError> {
 
     let networks = wifi.scan(true)?; // the argument forces a rescan or loading from cache
     let my_favorite_network = networks.first().unwrap();
-    let connected = wifi.connect(my_favorite_network.ssid.clone(), Some("my_password".to_string()))?;
+    let connected = wifi.connect(&my_favorite_network.ssid, Some("my_password"))?;
 
     if connected {
         wifi.disconnect()?;
     } else {
-        wifi.create(my_favorite_network.ssid.clone(), Some("password".to_string()))?;
+        wifi.create(&my_favorite_network.ssid, Some("password"))?;
     }
 
     Ok(())
